c1fce261f88d7a73fc2cc3bf334b412d9644ca0c
unknown <sac840711@gmail.com>
2014-12-02 16:58:33 +0800

    finish circle

---
+++ src/csclab/tw/Circle.java
@@ -0,0 +1,17 @@
+package csclab.tw;
+
+public class Circle extends Shape {
+	
+	private double radius;
+	
+	public Circle(double radius){
+		this.radius = radius;
+	}
+	
+	@Override
+	public double getArea() {
+		
+		return radius * radius * 3.14;
+	}
+
+}
